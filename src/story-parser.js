9547e3d3ef91208c810f1a30982d8e9e7cff52b0
Maëlys McArdle <maelys@localhost.localdomain>
2015-11-11 16:28:17 -0500

    In the middle of editing the parser.

--- src/story-parser.js
+++ src/story-parser.js
@@ -33,7 +33,7 @@ function parseStory( storyCode )
         "current paragraph": {
             "is choice": false,
             "choice id": "",
-            "content": "",
+            "text": "",
         }
         
         "current choice": {
@@ -49,23 +49,27 @@ function parseStory( storyCode )
         
         if ( trimmedLine.startsWith("[") ) {
             
+            // New choice link.
             story = endCurrentParagraph( story );
             story = addChoiceLink( story, trimmedLine );
             
         } else if ( trimmedLine.startsWith("###") ) {
             
+            // New choice/paragraph.
             story = endCurrentParagraph( story );
-            story = endCurrentChoice( story );
-            story = startNewChoice( story, trimmedLine );
+            story = startNewParagraph( story );
             
         } else if ( trimmedLine.startsWith("##") ) {
             
+            // New page/paragraph.
             story = endCurrentParagraph( story );
             story = endCurrentPage( story );
             story = startNewPage( story, trimmedLine );
+            story = startNewParagraph( story );
             
         } else if ( trimmedLine.startsWith("#") ) {
             
+            // New chapter.
             story = endCurrentParagraph( story );
             story = endCurrentPage( story );
             story = endCurrentChapter( story );
@@ -73,6 +77,7 @@ function parseStory( storyCode )
 
         } else {
             
+            // Continued paragraph.
             story = appendCurrentParagraph( story, line );
             
         }
@@ -87,8 +92,39 @@ function parseStory( storyCode )
     return story;
 }
 
+function everythingAfterSubstring( substring, line )
+{
+    return line.slice( substring.length ).trim();
+}
+
+function caseInsensitive( string )
+{
+    return string.toUpperCase()
+}
+
+function isChoiceStart( line )
+{
+    let choiceCode = everythingAfterSubstring( "###", line );
+    
+    if ( caseInsensitive( choiceCode ).startsWith( caseInsensitive( "Chose " ) ) {
+        
+       let choiceIdAndSource =  
+    
+    if ( choiceCode === "" ) {
+        return { 
+            "is choice": false, 
+            "choice id": null,
+            "choice target": null,
+        }
+    } else {
+    
+        
+    }
+}
+
 function startNewChapter( story, line )
 {
+    var chapterTitle = everythingAfterSubstring( "#", line );
 }
 
 function endCurrentChapter( story )
@@ -108,6 +144,10 @@ function startNewChoice( story, line )
 }
 
 function endCurrentChoice( story )
+{
+}
+
+function startNewParagraph( story )
 {
 }
 
